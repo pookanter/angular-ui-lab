0221d184d2fdd8951378fded860d1e0d501e7a46
rainstriker <cloneltorrer@gmail.com>
2024-04-20 14:56:05 +0700

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
   {
     path: "",
     pathMatch: "full",
-    redirectTo: "home",
-  },
-  {
-    path: "home",
-    loadComponent: () => import("./pages/home/home.component").then((m) => m.HomeComponent),
+    component: HomeComponent,
   },
   {
     path: "products",
