f1fd4ce0f263e269a7af2097cb43c4afc10b0e76
rainstriker <cloneltorrer@gmail.com>
2024-04-20 14:35:59 +0700

    test new routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home",
+  },
+  {
+    path: "home",
+    loadComponent: () => import("./pages/home/home.component").then((m) => m.HomeComponent),
+  },
+  {
+    path: "products",
+    loadComponent: () =>
+      import("./pages/products/products.component").then((m) => m.ProductsComponent),
+  },
+];
