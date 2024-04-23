cc7d50a9a24ec828772d81fdc6a0c4c39e1c1ead
rainstriker <cloneltorrer@gmail.com>
2024-04-23 21:20:25 +0700

    test grid

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
+import { CommonModule } from "@angular/common";
+import { IProducts } from "../../../../libs/types";
 
 @Component({
-  selector: 'app-products',
+  selector: "app-products",
   standalone: true,
-  imports: [],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  imports: [CommonModule, ProductCardComponent],
+  templateUrl: "./products.component.html",
+  styleUrl: "./products.component.css",
 })
 export class ProductsComponent {
-
+  products: IProducts.Product[] = [
+    {
+      id: 1,
+      name: "Red Running Shoes",
+      price: 3290,
+      quantity: 10,
+      image: "/assets/shoes/red-sneaker.jpg",
+      rating: 4,
+    },
+    {
+      id: 2,
+      name: "Indy Yeezy Shoes",
+      price: 5420,
+      quantity: 15,
+      image: "/assets/shoes/yeezy-sneaker.jpg",
+      rating: 3,
+    },
+    {
+      id: 3,
+      name: "Green Sport Shoes",
+      price: 2990,
+      quantity: 0,
+      image: "/assets/shoes/green-sneaker.jpg",
+      rating: 4.5,
+    },
+    {
+      id: 3,
+      name: "Pink Walking Shoes",
+      price: 1990,
+      quantity: 12,
+      image: "/assets/shoes/pink-sneaker.jpg",
+      rating: 3.5,
+    },
+  ];
 }
